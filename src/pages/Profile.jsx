be8ee25f81b9d3fe14b2df32e3d65b861845f638
Laura R. Ojeda <rojedalaura@gmail.com>
2018-10-26 17:56:17 +0200

    merge

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
 import song from '../services/song-service';
 import NavBar from '../components/NavBar';
-import LoadingAnimation from '../components/LoadingAnimation';
 
-<<<<<<< HEAD
-const Profile = () => {
-  return (
-    <div>
-      <NavBar />
-      <h1>Profile</h1>
-      <LoadingAnimation />
-    </div>
-  );
-};
-=======
 class Profile extends Component {
   state = {
     songList: [],
@@ -47,6 +35,5 @@ class Profile extends Component {
     );
   }
 }
->>>>>>> e0ff7236f02a7faa4f9d39c71954d29058a0996f
 
 export default Profile;
